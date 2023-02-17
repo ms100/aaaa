405a7e32dccefc51164ccb3f5f04ca3b69f9f114
GitHub Action <action@github.com>
2023-02-17 08:22:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/ms100/aaaa/MyBundle.kt
+++ src/main/kotlin/com/github/ms100/aaaa/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ms100.aaaa
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ms100/aaaa/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ms100/aaaa/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ms100.aaaa.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ms100.aaaa.MyBundle
 
 class MyProjectService(project: Project) {
 
